56654c4a4fab4e9044d000c4dcdab76e3207f30b
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-13 22:55:40 -0700

    Hands on class creation

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 'use strict'
 // spread
 
-function spread(){
-  let firstArray = ['c','d','e'];
-  let secondArray=['g','h'];
+function spread() {
+  let firstArray = ['c', 'd', 'e'];
+  let secondArray = ['g', 'h'];
   // In this example spread allows to expand the array by ...
-  console.log('spread results : ' + 'a','b',...firstArray,'f',...secondArray);
+  console.log('spread result : ' + 'a', 'b', ...firstArray, 'f', ...secondArray);
 }
 
 spread();
+// //
+//Class
+class Square {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  get area() {
+    return this.calculate();
+  }
+  calculate() {
+    return this.height * this.width;
+  }
+}
 
-//
+const squareInstance = new Square(10, 10);
+console.log('Class result :' + squareInstance.area);
+////
