30509f0449fad5a7297c9bc1ceccb266030e084d
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-13 22:49:14 -0700

    Understanding Es6

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+// spread
+
+function spread(){
+  let firstArray = ['c','d','e'];
+  let secondArray=['g','h'];
+  // In this example spread allows to expand the array by ...
+  console.log('spread results : ' + 'a','b',...firstArray,'f',...secondArray);
+}
+
+spread();
+
+//
